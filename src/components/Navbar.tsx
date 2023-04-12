03df719df515c9479657ed004c2f7f828b2b7edc
Justin Joseph <justin.joseph0@yahoo.com>
2023-04-11 20:52:09 -0400

    Build a navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { HStack, Image, Text } from '@chakra-ui/react';
+
+import logo from '../assets/logo.webp';
+
+function Navbar () {
+  return (
+    <HStack>
+      <Image src={logo} boxSize='5rem' />
+      <Text>Navbar</Text>
+    </HStack>
+  )
+}
+
+export default Navbar;
