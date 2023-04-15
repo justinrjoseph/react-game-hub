3b23f0756c010243f088be58bb3993a0b6920b07
Justin Joseph <justin.joseph0@yahoo.com>
2023-04-15 18:55:59 -0400

    Clean up game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,22 +19,17 @@ function GameGrid({ gameQuery }: Props): JSX.Element {
 
   const skeletons = Array.from({ length: 15 }, (_, i) => i)
 
-  if (gameQuery.genre && games.length === 0) {
-    return <p>No games match that genre.</p>;
-  }
+  if (error) return <Text>{error}</Text>
 
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
-        {loading && skeletons.map((skeleton) => {
-          return <GameCardSkeleton key={skeleton} />;
-        })}
-        {games && games.map((game) => {
-          return <GameCard key={game.id} game={game} />;
-        })}
-      </SimpleGrid>
-    </>
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
+      {loading && skeletons.map((skeleton) => {
+        return <GameCardSkeleton key={skeleton} />;
+      })}
+      {games && games.map((game) => {
+        return <GameCard key={game.id} game={game} />;
+      })}
+    </SimpleGrid>
   );
 }
 
