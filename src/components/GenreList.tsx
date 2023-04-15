00502668a85387d39284f39be0ec5e4d212c275d
Justin Joseph <justin.joseph0@yahoo.com>
2023-04-14 21:58:55 -0400

    Clean up genres list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from '@chakra-ui/react';
+import { Button, HStack, Heading, Image, List, ListItem, Spinner } from '@chakra-ui/react';
 
 import useGenres from '../hooks/useGenres';
 import { Genre } from '../models/genre';
@@ -18,19 +18,23 @@ function GenreList({ onSelectGenre, selectedGenre }: Props): JSX.Element | null 
 
   return (
     <>
+      <Heading fontSize="2xl" marginBlockEnd=".75rem">Genres</Heading>
       <List>
         {genres.map((genre) => {
           return <ListItem key={genre.id} marginY=".3125rem">
             <HStack>
               <Image src={getCroppedImageUrl(genre.image_background)}
                 boxSize="2rem"
-                borderRadius={8} />
+                borderRadius={8}
+                objectFit="cover" />
               <Button fontSize="lg"
                 fontWeight={selectedGenre?.id === genre.id
                   ? 'bold'
                   : 'normal'
                 }
                 variant="link"
+                whiteSpace="normal"
+                textAlign="left"
                 onClick={() => onSelectGenre(genre)}>
                 {genre.name}
               </Button>
